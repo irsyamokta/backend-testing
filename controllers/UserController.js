a09d8f32e6e061d4b2dea66fde70bbf8905246d4
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-23 18:38:16 +0700

    Add : UserController for get data users

---
+++ controllers/UserController.js
@@ -0,0 +1,24 @@
+import connection from "../config/connection.js";
+    
+const getUsers = async (req, res) => {
+    try{
+        const query = "SELECT * FROM users";
+        const [rows] = await connection.query(query);
+        if(rows.length === 0){
+            return res.status(404).json({message: "No users found"});
+        }
+        res.status(200).json({
+            status: "success",
+            data: {
+                users: rows
+            }
+        });
+    }catch(err){  
+        res.status(500).json({
+            status: "error",
+            message: err.message
+        });
+    }
+}
+
+export {getUsers};
