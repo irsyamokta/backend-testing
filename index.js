175787431e76ef1db566dfc410cac6e0b7a01d02
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-23 18:39:23 +0700

    Add : middleware to fetch endpoint

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connection from "./config/connection.js";
+import router from "./routes/user.js";
 
+dotenv.config();
 const app = express();
-let port = 3000
+let port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Server is running");
 })
 
-app.get("/api/testing", (req, res) => {
-    res.json(
-        {
-            "name": "Testing API",
-            "message": "Success"
-        }
-    );
-})
+app.use("/", router);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    console.log(`http://localhost:${port}`);
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.error(`Error connecting to the database: ${err.stack}`)
+        return
+    }
+    console.log(`Connected to the database as id ${connection.threadId}`)
 })
