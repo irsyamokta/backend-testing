aac8c223df2148af5e69a2ee80224dfe5b0b8b97
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-23 18:37:52 +0700

    Add : Database connection

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+import mysql from "mysql"
+import util from "util"
+import dotenv from "dotenv"
+dotenv.config()
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+connection.query = util.promisify(connection.query)
+
+export default connection
+
